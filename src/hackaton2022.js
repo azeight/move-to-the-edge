ac3f7677148378d2ad67e1083355e15b4afc5451
Murilo Andrade <murilo.andrade@azion.com>
2022-05-02 17:08:02 -0300

    Revert "Live code"
    This reverts commit 06e8e7b1b09ed90f86a1dc3ffa27d7c8ba66f678.

--- src/hackaton2022.js
+++
@@ -1,8 +0,0 @@
-async function handleRequest(request) {
-  return new Response("live code hackaton2022")
-}
-
-addEventListener("fetch", event => {
-  return event.respondWith(handleRequest(event.request))
-})
-
