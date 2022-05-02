c14b9907b8b2dfd4557f11a10c03b864e074f276
Murilo Andrade <murilo.andrade@azion.com>
2022-05-02 16:07:28 -0300

    Revert "Use the azeight soluction in live"
    This reverts commit 900c0701e9b93f056fceb5104680e950390c3154.

--- src/hackaton2022.js
+++
@@ -1,8 +0,0 @@
-async function handleRequest(request) {
-  return new Response("Hackaton2022 = azeight the best hackaton team!")
-}
-
-addEventListener("fetch", event => {
-  return event.respondWith(handleRequest(event.request))
-})
-
