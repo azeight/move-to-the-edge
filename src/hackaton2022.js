42256b12f55f2a2f6a337b62ed12d1c8e3f87722
Murilo Andrade <murilo.andrade@azion.com>
2022-05-02 15:35:39 -0300

    Revert livecode test function

--- src/hackaton2022.js
+++
@@ -1,8 +0,0 @@
-async function handleRequest(request) {
-  return new Response("The best hackaton team is AzEight!")
-}
-
-addEventListener("fetch", event => {
-  return event.respondWith(handleRequest(event.request))
-})
-
